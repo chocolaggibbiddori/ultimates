fb90ee14085e8fcea1a9dd37c15b5819beb4c7f2
rlojun <cjun0117@hanmail.net>
2023-09-20 18:26:35 +0900

    Refactor : getRecord 메서드의 매개변수명 변경
    gameId -> recordId 로 변경

--- rss/src/main/java/com/ultimates/rss/service/RecordService.java
+++ rss/src/main/java/com/ultimates/rss/service/RecordService.java
@@ -2,14 +2,14 @@ package com.ultimates.rss.service;
 
 import com.ultimates.rss.dto.Champ;
 import com.ultimates.rss.dto.MostChamp;
-import com.ultimates.rss.dto.Record;
+import com.ultimates.rss.dto.RecordDetail;
 import com.ultimates.rss.dto.RecordList;
 
 import java.util.List;
 
 public interface RecordService {
 
-    Record getRecord(String username, int gameId);
+    RecordDetail getRecord(String username, int recordId);
 
     List<RecordList> getRecords(String username);
 
